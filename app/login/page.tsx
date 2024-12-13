6c897a17ca0a76a4f773c1ebf47e942bc1e60bd2
akkiaryan <akkiaryan777@gmail.com>
2024-12-13 15:04:05 +0530

    completed the login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Card,CardContent,CardDescription, CardHeader,CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+
 export default function Login(){
     return (
-        <div>
-            <h1>Hello From Login Page</h1>
-        </div>
+       <>
+       <div className="flex h-screen w-full items-center justify-center px-4">
+        <Card className = "max-w-sm">
+            <CardHeader>
+                <CardTitle className="text-2xl">Login</CardTitle>
+                <CardDescription>Enter your email below to Login</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form className="flex flex-col gap-y-4">
+                    <div className="flex flex-col gap-y-2">
+                        <Label>Email</Label>
+                        <Input placeholder="hello@example.com" />
+                    </div>
+                    <Button>Submit</Button>
+                </form>
+            </CardContent>
+        </Card>
+
+       </div>
+       </>
     )
 }
